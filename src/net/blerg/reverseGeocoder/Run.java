5eb4c5c6bb30065808ccacb6b72d23ee5a51b1a4
Arthur Embleton <arthur@blerg.net>
2011-11-04 01:08:09 +0000

    Made a pathetically small start

---
+++ src/net/blerg/reverseGeocoder/Run.java
@@ -0,0 +1,17 @@
+package net.blerg.reverseGeocoder;
+
+public class Run {
+
+	private static final String WIKIPEDIA="http://en.wikipedia.org/wiki/";
+	private static final String LIST_OF_LOCATIONS="List_of_United_Kingdom_locations";
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		//read in the list of locations
+		
+	}
+
+}
